d07e3e38032940af0f44c308f2b502d975999771
seth4llm <seth4llm@gmail.com>
2014-03-21 14:58:58 -0500

    full design functionality
    compression removes comments, regex corrections/optimization

--- CruSShr.js
+++ CruSShr.js
@@ -113,6 +113,26 @@ var runtime = function() {
 };
 
 */
+var ByteSize = function ( number, precision, bits, base ) {
+	if ( typeof _ == 'undefined' ) { _ = ByteSize };
+	if ( typeof _.prefix == 'undefined' ) { _.prefix = [
+		'', 'K', 'M', 'G', 'T', 'P', 'E', 'Z', 'Y'
+		] };
+	var useBits = ( bits || false );
+	var usePrecision = Math.pow( 10, ( precision || 2 ));
+	var useBase = ( base || 1000 );
+
+	var p = 0;
+	while ( number / Math.pow( useBase, p ) > useBase ) { p++ };
+	var result = Math.round( ( number / Math.pow( useBase, p )) * usePrecision ) / usePrecision;
+	return result + ' ' + _.prefix[p] + ( useBase == 1024 && p > 0 ? 'i' : '' ) + ( useBits ? 'b' : 'B' );
+};
+
+var Thousands = function ( number ) {
+	var t = 0, n = '' + number;
+	while ( number / Math.pow( 1000, t ) >= 1000 ) { t++ };
+	return t;
+};
 
 /* AngularJS section */
 var CruSShr = function ( $scope ) {
@@ -121,16 +141,16 @@ var CruSShr = function ( $scope ) {
 	$scope.CruSSh_it = function ( ) {
 		var start = new Date();
 		var unprocessed = $scope.UnCruSShd.content;
-		$scope.CruSShd.content = unprocessed.replace( /\/\*[^\*]+?\*\//gm, '' )
+		$scope.UnCruSShd.len = ByteSize ( $scope.UnCruSShd.content.length );
+		$scope.CruSShd.content = unprocessed.replace( /[\n\r\t]/g, '' )
 			.replace( /(\ {2,})/g, ' ' )
-			.replace( /([\n\r]{2,})/g, '\n' )
-			.replace( /\s?([;:\.\{\},])\s?/g, '$1' )
+			.replace( /\/\*.+?\*\//gm, '' )
+			.replace( /\s?([;:\.\{\},!])\s?/g, '$1' )
 			.replace( /([^0-9])0(\.\d+)/g, '$1$2' )
 			.replace( /(#(\d)\2(\d)\3(\d)\4)/g, '#$2$3$4' )
-			.replace( /\t/g, '' )
 			.replace( /\;\}/g, '}' );
 		var end = new Date();
-		$scope.CruSShd.len = $scope.CruSShd.content.length;
+		$scope.CruSShd.len = ByteSize ( $scope.CruSShd.content.length );
 		$scope.CruSShd.lps = end - start;
 	};
 };
